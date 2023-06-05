753c22ff205cd699448b6de1468b3fdf1475842f
Victor M <virtus.mv@gmail.com>
2023-06-05 12:53:23 +0700

    Rename crossword to wordsearch

--- wordsearch.py
+++ wordsearch.py
@@ -1,7 +1,7 @@
-#!/home/victor/python_projects/crosswords/venv/bin/python3
+#!/home/victor/python_projects/wordsearch/venv/bin/python3
 import numpy as np                      #numpy
 import copy                             #for deep copy
-from string import ascii_lowercase      #for getting random ascii characters to fill the crossword
+from string import ascii_lowercase      #for getting random ascii characters to fill the wordsearch
 
 # reportlab imports for working with PDFs
 from reportlab.lib.pagesizes import letter
@@ -26,86 +26,86 @@ class Tile:
         else:
             return False
 
-class Crossword:
+class Wordsearch:
     def __init__(self, dimension_x, dimension_y):
         # sets a basic list of lists given some dirmensions
         self.dimension_x=dimension_x 
         self.dimension_y=dimension_y
-        self.crossword=self.create_empty_crossword()
-        self.def_value = self.crossword[0][0].value
+        self.wordsearch=self.create_empty_wordsearch()
+        self.def_value = self.wordsearch[0][0].value
     
-    def create_empty_crossword(self):
-        # generates an empty crossword given some dimensions
+    def create_empty_wordsearch(self):
+        # generates an empty wordsearch given some dimensions
         #  x * y (ys are rows and xs are columns)
         ys = self.dimension_y
         xs = self.dimension_x
-        crossword = []
+        wordsearch = []
         for y in range(ys):
-            # for loop to fill in the crossword with empty tiles.
+            # for loop to fill in the wordsearch with empty tiles.
             temp = []
             for x in range(xs):
                 temp.append(Tile())
-            crossword.append(temp)
-        return crossword
+            wordsearch.append(temp)
+        return wordsearch
         
-    def print_crossword(self):
+    def print_wordsearch(self):
         """
-        prints the crossword to the terminal in an understandable way
+        prints the wordsearch to the terminal in an understandable way
         """
-        for y in range(len(self.crossword)):
-            for tile in self.crossword[y]:
+        for y in range(len(self.wordsearch)):
+            for tile in self.wordsearch[y]:
                 print(tile.value, end="  ")
             print()
         print()
 
     def place_word_h(self, word, loc_x, loc_y, backwards=False):
-        """Place a word in the crossword, horizontaly"""
+        """Place a word in the wordsearch, horizontaly"""
         len_word=len(word)
         # check if the word fits in the selected location
         if loc_x+len_word>self.dimension_x:
             print('word is too big!')
             return False
         else:
-            # dummy crossword to which we make changes before commiting to the real one
+            # dummy wordsearch to which we make changes before commiting to the real one
             # deepcopy because otherwise we copy the reference, and we dont want that
-            fake_crossword = copy.deepcopy(self.crossword)
+            fake_wordsearch = copy.deepcopy(self.wordsearch)
             # inverts the word
             if backwards:
                 word=word[::-1]
             
-            # variable to keep track of the location in the crossword 
+            # variable to keep track of the location in the wordsearch 
             temp_loc_x=0
             for char in word:
-                if fake_crossword[loc_y][loc_x+temp_loc_x].value == self.def_value or fake_crossword[loc_y][loc_x+temp_loc_x].value == char:
-                    fake_crossword[loc_y][loc_x+temp_loc_x].value = char.upper()
+                if fake_wordsearch[loc_y][loc_x+temp_loc_x].value == self.def_value or fake_wordsearch[loc_y][loc_x+temp_loc_x].value == char:
+                    fake_wordsearch[loc_y][loc_x+temp_loc_x].value = char.upper()
                     temp_loc_x += 1
                 else:
                     return False
             # if the assignation of characters is complete,
-            # point the self.crossword to the fake_crossword
-            self.crossword = fake_crossword
+            # point the self.wordsearch to the fake_wordsearch
+            self.wordsearch = fake_wordsearch
             return True
 
     def place_word_v(self, word, loc_x, loc_y, backwards=False):
-        """Place a word in the crossword, horizontaly"""
+        """Place a word in the wordsearch, horizontaly"""
         len_word=len(word)
         if loc_y+len_word>self.dimension_y:
             print('word is too big!')
             return False
         else:
-            fake_crossword = copy.deepcopy(self.crossword)
+            fake_wordsearch = copy.deepcopy(self.wordsearch)
             if backwards:
                 word=word[::-1]
             temp_loc_y=0
             for char in word:
-                # tile = self.crossword[loc_y][loc_x+temp_loc_x]
+                # tile = self.wordsearch[loc_y][loc_x+temp_loc_x]
                 # print(f'tile: {tile}\tdefvalue: {self.def_value}')
-                if fake_crossword[loc_y+temp_loc_y][loc_x].value == self.def_value or  fake_crossword[loc_y+temp_loc_y][loc_x].value == char:
-                    fake_crossword[loc_y+temp_loc_y][loc_x].value = char.upper()
+                if fake_wordsearch[loc_y+temp_loc_y][loc_x].value == self.def_value or  fake_wordsearch[loc_y+temp_loc_y][loc_x].value == char:
+                    fake_wordsearch[loc_y+temp_loc_y][loc_x].value = char.upper()
                     temp_loc_y += 1
                 else:
                     return False
-            self.crossword=fake_crossword
+            self.wordsearch=fake_wordsearch
             return True
     
     def place_word_d1(self, word, loc_x, loc_y, backwards=False):
@@ -118,23 +118,23 @@ class Crossword:
             print('word is too big')
             return False
         else:
-            fake_crossword = copy.deepcopy(self.crossword)
+            fake_wordsearch = copy.deepcopy(self.wordsearch)
             if backwards:
                 word = word[::-1]
             temp_loc_y = 0
             temp_loc_x = 0
             print(f'adding word {word}')
             for char in word:
-                if fake_crossword[loc_y+temp_loc_y][loc_x+temp_loc_x].value == self.def_value or fake_crossword[loc_y+temp_loc_y][loc_x+temp_loc_x].value == char:
-                    fake_crossword[loc_y+temp_loc_y][loc_x+temp_loc_x].value = char.upper()
+                if fake_wordsearch[loc_y+temp_loc_y][loc_x+temp_loc_x].value == self.def_value or fake_wordsearch[loc_y+temp_loc_y][loc_x+temp_loc_x].value == char:
+                    fake_wordsearch[loc_y+temp_loc_y][loc_x+temp_loc_x].value = char.upper()
                     temp_loc_x += 1
                     temp_loc_y += 1
                     print(f'adding char {char}')
                 else:
                     print(f'failed to add char {char}')
                     return False
-            self.crossword = fake_crossword
-            self.print_crossword()
+            self.wordsearch = fake_wordsearch
+            self.print_wordsearch()
             return True
     
     def place_word_d2(self, word, loc_x, loc_y, backwards=False):
@@ -147,7 +147,7 @@ class Crossword:
             print('word is too big')
             return False
         else:
-            fake_crossword = copy.deepcopy(self.crossword)
+            fake_wordsearch = copy.deepcopy(self.wordsearch)
             if backwards:
                 word = word[::-1]
             temp_loc_y = 0
@@ -155,8 +155,8 @@ class Crossword:
             print(f'adding word {word}')
             for char in word:
                 # try:
-                if fake_crossword[loc_y + temp_loc_y][temp_loc_x].value == self.def_value or fake_crossword[loc_y+temp_loc_y][temp_loc_x].value == char:
-                    fake_crossword[loc_y+temp_loc_y][temp_loc_x].value = char.upper()
+                if fake_wordsearch[loc_y + temp_loc_y][temp_loc_x].value == self.def_value or fake_wordsearch[loc_y+temp_loc_y][temp_loc_x].value == char:
+                    fake_wordsearch[loc_y+temp_loc_y][temp_loc_x].value = char.upper()
                     temp_loc_x -= 1
                     temp_loc_y += 1
                     print(f'adding char {char}')
@@ -165,9 +165,9 @@ class Crossword:
                     return False
                 # except:
                 #     print(f'unable to add word: {word}')
-                #     self.print_crossword()
-            self.crossword = fake_crossword
-            self.print_crossword()
+                #     self.print_wordsearch()
+            self.wordsearch = fake_wordsearch
+            self.print_wordsearch()
             return True
         
     def get_random_loc(self, x=None, y=None):
@@ -184,24 +184,25 @@ class Crossword:
 
     def fill_empty_places(self):
         # fill the spaces with the default value with ascii lowercase characters.
-        for i in range(len(self.crossword)):
-            for j in range(len(self.crossword[i])):
-                if self.crossword[i][j].value == self.def_value:
-                    self.crossword[i][j].value = np.random.choice([x.upper() for x in ascii_lowercase])
+        for i in range(len(self.wordsearch)):
+            for j in range(len(self.wordsearch[i])):
+                if self.wordsearch[i][j].value == self.def_value:
+                    self.wordsearch[i][j].value = np.random.choice([x.upper() for x in ascii_lowercase])
 
-    def addWords(self, file_name, difficulty = 1):
-        with open('/home/victor/python_projects/crosswords/word_list_translations.txt') as f:
+    def addWords(self, txt_file, difficulty = 1):
+        with open(txt_file) as f:
              lines = f.readlines()
 
         lines = [line.strip() for line in lines]
         # depending on the file, it will try to extract the first word befor the '-' char 
         words = [re.findall(r'(.*?) - \(.*\)', line) for line in lines]
+        if len(words[0]) == 0:
+            words = lines
+        print(len(words))
         if type(words[0]) is list:
             words = [x[0] for x in words ]
-        
-        print(words)
 
-        # Here is the actual filling of the crossword.
+        # Here is the actual filling of the wordsearch.
         max_attempts = 1000
         failed_words = []
         for word in words:
@@ -219,7 +220,7 @@ class Crossword:
                         # vertical
                         flag = self.place_word_v(word, loc_x, loc_y)
                     if flag:
-                        self.print_crossword()
+                        self.print_wordsearch()
                         break
             
                 # difficulty of 2, i.e. medium
@@ -236,7 +237,7 @@ class Crossword:
                         # diagonal 1
                         flag = self.place_word_d1(word, loc_x, loc_y)
                     if flag:
-                        self.print_crossword()
+                        self.print_wordsearch()
                         break
 
                 # difficulty of 3, i.e. hard
@@ -256,7 +257,7 @@ class Crossword:
                         # diagonal 2
                         flag = self.place_word_d2(word, loc_x, loc_y)
                     if flag:
-                        self.print_crossword()
+                        self.print_wordsearch()
                         break
 
                 # difficulty of 4, i.e. very hard
@@ -277,7 +278,7 @@ class Crossword:
                         # diagonal 2
                         flag = self.place_word_d2(word, loc_x, loc_y, backwards=backwards)
                     if flag:
-                        self.print_crossword()
+                        self.print_wordsearch()
                         break
                 
 
@@ -288,7 +289,7 @@ class Crossword:
         print(f'words attepted: {len(words)}\twords failed: {len(failed_words)}')
         print(failed_words)
         self.fill_empty_places()
-        self.print_crossword()
+        self.print_wordsearch()
 
 class PDFHelper:
     def __init__(self):
@@ -346,9 +347,9 @@ class PDFHelper:
                         # hAlign = 'LEFT')
         return table
 
-    def create_crossword(self, crossword, txt_file, filename='word_search.pdf', title = ''):
+    def create_wordsearch(self, wordsearch, txt_file, filename='word_search.pdf', title = ''):
         """
-        Takes a crossword (an instance of the crossword class) and a file name (word_search.pdf as default)
+        Takes a wordsearch (an instance of the wordsearch class) and a file name (word_search.pdf as default)
         """
         # initialize the SimpleDocTemplate with som standard values
         doc = SimpleDocTemplate(filename,
@@ -357,13 +358,13 @@ class PDFHelper:
                             topMargin=0.4*inch,
                             rightMargin=0.4*inch,
                             leftMargin=0.4*inch)
-        # array used to store the values of the tiles of the crossword 
+        # array used to store the values of the tiles of the wordsearch 
         temp=[]
         # array used to store the arrays to be used as rows in the table
         tab=[]
-        for row in range(len(crossword.crossword)):
-            for col in range(len(crossword.crossword[row])):
-                temp.append(crossword.crossword[row][col].value)
+        for row in range(len(wordsearch.wordsearch)):
+            for col in range(len(wordsearch.wordsearch[row])):
+                temp.append(wordsearch.wordsearch[row][col].value)
             tab.append(temp)
             temp=[] 
 
@@ -378,10 +379,10 @@ class PDFHelper:
         # styles.add(ParagraphStyle(name='centered', alignment=TA_CENTER))
         t0 = Paragraph(title, titleStyle)
         
-        # print(f'col={crossword.dimension_x}\trow={crossword.dimension_y}\ttype={type(crossword.dimension_x)}')
+        # print(f'col={wordsearch.dimension_x}\trow={wordsearch.dimension_y}\ttype={type(wordsearch.dimension_x)}')
 
         # the colWidths and rowHeights are difined as the space beteween cells, and take an array such as [0.2*inch, 0.2*inch ...]
-        t1 = Table(tab, colWidths=[0.3*inch]*crossword.dimension_x, rowHeights=[0.3*inch]*crossword.dimension_y) # dimensions required
+        t1 = Table(tab, colWidths=[0.3*inch]*wordsearch.dimension_x, rowHeights=[0.3*inch]*wordsearch.dimension_y) # dimensions required
         t1.setStyle(TableStyle([('BOX', (0,0), (-1,-1), 2, colors.black), 
                                 ('FONT', (0,0),(-1,-1),'Helvetica', 12),
                                 ('ALIGN', (0, 0), (-1, -1), "CENTER")]))
@@ -401,14 +402,23 @@ class PDFHelper:
 
 
 if __name__ == "__main__":
-    # set dimensions of the crossword (max reasonable dimensions 22, 32 for a whole page)
-    cw = Crossword(22, 30)
-    # print('empty crossword')
-    cw.print_crossword()
-    txt_file = '/home/victor/python_projects/crosswords/word_list_translations.txt'
+    # set dimensions of the wordsearch (max reasonable dimensions 22, 32 for a whole page)
+    cw = Wordsearch(22, 30)
+    # print('empty wordsearch')
+    cw.print_wordsearch()
+    # txt_file = '/home/victor/python_projects/wordsearch/word_list_translations.txt'
+    txt_file = '/home/victor/python_projects/wordsearch/word_list_places.txt'
 
     cw.addWords( txt_file, difficulty=4)
+    difficulty = 1
 
     pdf_helper = PDFHelper()
-    pdf_helper.create_crossword(cw, txt_file, title='WordSearch' )
+    pdf_helper.create_wordsearch(cw, txt_file, title='WordSearch' )
+
+    for i in range(15):
+        cw = Wordsearch(18, 25)
+        cw.addWords( txt_file, difficulty=difficulty)
+
+        pdf_helper.create_wordsearch(cw, txt_file, title='WordSearch', filename=f'word_search_{str(difficulty)}_{i:02}.pdf' )
+
     
